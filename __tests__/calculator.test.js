705102d40a65b891061d3445a3ddeb4432bdfaae
taylulz <taylulzcode@gmail.com>
2020-11-13 16:48:53 -0800

    purposefully fail refactored jupiter() test for life expectancy and years left

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -21,16 +21,18 @@ describe('Calculator', () => {
     expect(calculator.venusAge).toEqual(48)
   });
 
-  test('should correctly convert earthAge to marsAge', () => {
+  test('should correctly convert earthAge to marsAge, marsExp, and marsYearsLeft', () => {
     calculator.mars();
     expect(calculator.marsAge).toEqual(16);
     expect(calculator.marsExp).toEqual(43);
     expect(calculator.marsYearsLeft).toEqual(27);
   });
 
-  test('should correctly convert earthAge to jupiterAge', () => {
+  test('should correctly convert earthAge to jupiterAge, jupiterExp, and jupiterYearsLeft', () => {
     calculator.jupiter();
     expect(calculator.jupiterAge).toEqual(3);
+    expect(calculator.jupiterExp).toEqual(7);
+    expect(calculator.jupiterYearsLeft).toEqual(3);
   });
 
   test('should correctly take in user life expectancy', () => {
@@ -52,10 +54,10 @@ describe('Calculator', () => {
     expect(calculator.venusExp).toEqual(129);
   });
 
-  test('should correctly calculate earth life expectancy to Jupiter life expectancy', () => {
-    calculator.jupiter();
-    expect(calculator.jupiterExp).toEqual(7);
-  });
+  // test('should correctly calculate earth life expectancy to Jupiter life expectancy', () => {
+  //   calculator.jupiter();
+  //   expect(calculator.jupiterExp).toEqual(7);
+  // });
 
   test('should correctly calculate years left to live on Mercury', () => {
     calculator.mercury();
@@ -67,15 +69,10 @@ describe('Calculator', () => {
     expect(calculator.venusYearsLeft).toEqual(81);
   });
 
-  test('should correctly calculate years left to live on Mars', () => {
-    calculator.mars();
-    expect(calculator.marsYearsLeft).toEqual(27);
-  });
-
-  test('should correctly calculate years left to live on Jupiter', () => {
-    calculator.jupiter();
-    expect(calculator.jupiterYearsLeft).toEqual(4);
-  });
+  // test('should correctly calculate years left to live on Jupiter', () => {
+  //   calculator.jupiter();
+  //   expect(calculator.jupiterYearsLeft).toEqual(4);
+  // });
 
   test('should correctly calculate number of years a person has lived past earth life expectancy', () => {
     calculator.earthAge = 90;
